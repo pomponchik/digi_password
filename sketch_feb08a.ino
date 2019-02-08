264c3b6a5671355136d510051db0653b5cc01836
pomponchik <34486915+pomponchik@users.noreply.github.com>
2019-02-09 02:07:05 +0300

    Add files via upload

---
+++ sketch_feb08a.ino
@@ -0,0 +1,14 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.println("login\tpassword");
+  DigiKeyboard.delay(5);
+  DigiKeyboard.println("\n");
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.println("\n");
+}
+
+void loop() {
+  
+}
